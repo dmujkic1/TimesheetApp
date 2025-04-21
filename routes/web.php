ceb079e35f87e1a40e26821567844c1cefd7b5ae
dmujkic1 <dmujkic1@etf.unsa.ba>
2025-04-21 10:44:34 +0200

    auth middleware in routes

--- routes/web.php
+++ routes/web.php
@@ -17,23 +17,19 @@ Route::get('dashboard', function () {
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
 
-Route::prefix('employees')->group(function () {
-    Route::get('add', [EmployeeController::class, 'create'])->name('employees.create'); // Prikaz forme
-    Route::post('store', [EmployeeController::class, 'store'])->name('employees.store'); // Slanje forme
-    Route::get('getAll', [EmployeeController::class, 'index'])->name('employees.index');
-    Route::get('get/{employeeId}', [EmployeeController::class, 'show'])->name('employees.show');
-    Route::get('edit/{employeeId}', [EmployeeController::class, 'edit'])->name('employees.edit');
-    Route::put('modify/{employeeId}', [EmployeeController::class, 'update'])->name('employees.update');
-    Route::delete('delete/{employeeId}', [EmployeeController::class, 'destroy'])->name('employees.destroy');
+Route::middleware(['auth'])->group(function () {
+    Route::prefix('employees')->group(function () {
+        Route::get('add', [EmployeeController::class, 'create'])->name('employees.create');
+        Route::post('store', [EmployeeController::class, 'store'])->name('employees.store');
+        Route::get('getAll', [EmployeeController::class, 'index'])->name('employees.index');
+        Route::get('get/{employeeId}', [EmployeeController::class, 'show'])->name('employees.show');
+        Route::get('edit/{employeeId}', [EmployeeController::class, 'edit'])->name('employees.edit');
+        Route::put('update/{employeeId}', [EmployeeController::class, 'update'])->name('employees.update');
+        Route::delete('delete/{employee}', [EmployeeController::class, 'destroy'])->name('employees.destroy');
+    });
 
-});
-
-Route::prefix('managers')->group(function(){
-    Route::get('add', [ManagerController::class, 'create'])->name('managers.create');
-    Route::get('getAll', [ManagerController::class, 'index'])->name('managers.index');
-
-
-    Route::put('update/{employeeId}', [EmployeeController::class, 'update'])->name('employees.update');
-    Route::delete('delete/{employee}', [EmployeeController::class, 'destroy'])->name('employees.destroy');
-
+    Route::prefix('managers')->group(function () {
+        Route::get('add', [ManagerController::class, 'create'])->name('managers.create');
+        Route::get('getAll', [ManagerController::class, 'index'])->name('managers.index');
+    });
 });
