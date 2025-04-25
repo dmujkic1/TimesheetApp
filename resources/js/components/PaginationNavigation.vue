67e33461e4b539daeb1e089789c73f8672f39068
dmujkic1 <dmujkic1@etf.unsa.ba>
2025-04-25 11:30:24 +0200

    Employee & Manager pagination with PaginationNavigation.vue component logic

---
+++ resources/js/components/PaginationNavigation.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="flex justify-center items-center mt-6 mb-20 space-x-4">
+      <!-- PRETHODNA -->
+      <div class="w-[130px] flex justify-center">
+        <button
+          v-if="pagination.prev_page_url"
+          @click="changePage(pagination.current_page - 1)"
+          class="w-full text-center px-3 py-1 bg-purple-200 hover:bg-purple-300 text-purple-800 rounded"
+        >
+          ◀ Prethodna
+        </button>
+        <div v-else class="px-3 py-1 invisible">◀ Prethodna</div>
+      </div>
+  
+      <!-- Brojevi stranica -->
+      <div class="flex space-x-2 justify-center">
+        <button
+          v-for="page in pagination.last_page"
+          :key="page"
+          @click="changePage(page)"
+          :class="[ 
+            'px-3 py-1 rounded',
+            page === pagination.current_page
+              ? 'bg-purple-600 text-white'
+              : 'bg-purple-100 hover:bg-purple-200 text-purple-800'
+          ]"
+        >
+          {{ page }}
+        </button>
+      </div>
+  
+      <!-- SLJEDEĆA -->
+      <div class="w-[130px] flex justify-center">
+        <button
+          v-if="pagination.next_page_url"
+          @click="changePage(pagination.current_page + 1)"
+          class="w-full text-center px-3 py-1 bg-purple-200 hover:bg-purple-300 text-purple-800 rounded"
+        >
+          Sljedeća ▶
+        </button>
+        <div v-else class="px-3 py-1 invisible">Sljedeća ▶</div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  defineProps({
+    pagination: {
+      type: Object,
+      required: true
+    },
+    changePage: {
+      type: Function,
+      required: true
+    }
+  })
+  </script>
+  
--- resources/js/pages/web/managers/Index.vue
+++ resources/js/pages/web/managers/Index.vue
@@ -60,48 +60,10 @@
     </ul>
 
     <!-- Navigacija kroz paginaciju -->
-    <div class="flex justify-center items-center mt-6 mb-20 space-x-4">
-      <!-- PRETHODNA -->
-      <div class="w-[130px] flex justify-center">
-        <button
-          v-if="pagination.prev_page_url"
-          @click="changePage(pagination.current_page - 1)"
-          class="w-full text-center px-3 py-1 bg-purple-200 hover:bg-purple-300 text-purple-800 rounded"
-        >
-          ◀ Prethodna
-        </button>
-        <div v-else class="px-3 py-1 invisible">◀ Prethodna</div>
-      </div>
-
-      <!-- Brojevi stranica -->
-      <div class="flex space-x-2 justify-center">
-        <button
-          v-for="page in pagination.last_page"
-          :key="page"
-          @click="changePage(page)"
-          :class="[
-            'px-3 py-1 rounded',
-            page === pagination.current_page
-              ? 'bg-purple-600 text-white'
-              : 'bg-purple-100 hover:bg-purple-200 text-purple-800'
-          ]"
-        >
-          {{ page }}
-        </button>
-      </div>
-
-      <!-- SLJEDEĆA -->
-      <div class="w-[130px] flex justify-center">
-        <button
-          v-if="pagination.next_page_url"
-          @click="changePage(pagination.current_page + 1)"
-          class="w-full text-center px-3 py-1 bg-purple-200 hover:bg-purple-300 text-purple-800 rounded"
-        >
-          Sljedeća ▶
-        </button>
-        <div v-else class="px-3 py-1 invisible">Sljedeća ▶</div>
-      </div>
-    </div>
+    <PaginationNavigation
+      :pagination="pagination"
+      :changePage="changePage"
+    />
 
 
     <!-- Modal -->
@@ -161,6 +123,7 @@
 </template>
 
 <script setup>
+import PaginationNavigation from '@/components/PaginationNavigation.vue'
 import Navbar from '@/components/Navbar.vue'
 import Footer from '@/components/Footer.vue'
 import { ref } from 'vue'
@@ -188,7 +151,10 @@ const openModal = (manager) => {
 }
 
 const changePage = (page) => {
-  router.visit(route('managers.index', { page }), {
+  router.visit(route('managers.index', {
+      page: page,
+      search: props.search // Očuvaj trenutni search query prilikom paginacije
+    }), {
     preserveState: true,
     preserveScroll: true,
   })
