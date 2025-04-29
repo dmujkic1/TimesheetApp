540b944479346effcbd7b0b41be26aaf9a43c2ed
dmujkic1 <dmujkic1@etf.unsa.ba>
2025-04-29 10:11:30 +0200

    timesheets DB table

---
+++ app/Http/Controllers/TimeSheetController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TimeSheet;
+use Illuminate\Http\Request;
+
+class TimeSheetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TimeSheet $timeSheet)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TimeSheet $timeSheet)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, TimeSheet $timeSheet)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TimeSheet $timeSheet)
+    {
+        //
+    }
+}
